72af8529158a0f8aa5d4094e4cb35ea6772d7f5f
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-06-29 10:14:59 +0530

    SingleToggle accordion component created

--- react-accordions/src/App.js
+++ react-accordions/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Accordion1 from './components/accordion1/SingleOpen';
+import SingleToggle from './components/accordion2/SingleToggle';
 
 function App() {
   return (
     <div className="App">
       <h3>Accordions</h3>
       <Accordion1 />
+      <SingleToggle />
     </div>
   );
 }
