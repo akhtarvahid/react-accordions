0517f4aa6b930e1ea993c3f667c4cbdb4cbe9c08
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-06-30 10:17:23 +0530

    json id used instead of map index

---
+++ react-accordions/src/components/accordion1/SingleOpen.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './SingleOpen.css';
+import listArray from "../../utils/data.json";
+import AccordionHeader from './AccordionHeader';
+export default class Accordion1 extends React.Component {
+  state = {
+    lists: listArray,
+    selectedIndx: null,
+  };
+  handleTile = (index) => {
+    this.setState({ selectedIndx: index });
+  };
+
+  render() {
+    const { selectedIndx, lists } = this.state;
+    return (
+      <>
+        {lists.map((list) => (
+          <div
+            className="heading"
+            key={list.id}
+            onClick={() => this.handleTile(list.id)}
+          >
+            <AccordionHeader list={list} selectedIndx={selectedIndx} index={list.id} />
+            {selectedIndx === list.id ? (
+              <div className="content">
+                <div>{list.content}</div>
+              </div>
+            ) : null}
+          </div>
+        ))}
+      </>
+    );
+  }
+}
