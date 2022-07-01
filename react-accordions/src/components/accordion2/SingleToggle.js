aeed46c93cbd4cf0489da65e8a3a730a3403e03f
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-07-01 16:13:27 +0530

    style added

--- react-accordions/src/components/accordion2/SingleToggle.js
+++ react-accordions/src/components/accordion2/SingleToggle.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import listArray from "../../utils/data.json";
-
+import './SingleToggle.css';
 export default class SingleToggle extends Component {
     state = {
         lists: []
@@ -12,16 +12,19 @@ export default class SingleToggle extends Component {
     }
     selectedRowHandler=(row)=> {
       this.setState({
-          lists: this.state.lists.map(list => list.id === row.id ? ({...list, open: true }): list)
+          lists: this.state.lists.map(list => list.id === row.id ? ({...list, open: !list.open }): list)
       })
     }
     render() {
         return (
             <div>
               {this.state.lists.map(list => 
-              <div key={list.id} onClick={() => this.selectedRowHandler(list)}>
-                 <div>{list.title}</div>
-                 {list.open && <div>Opened</div>} 
+              <div key={list.id}  onClick={() => this.selectedRowHandler(list)}>
+                 <div className="acc-heading">
+                    <div>{list.title}</div>
+                    <div>V</div>
+                 </div>
+                 {list.open && <div className="content">Opened</div>} 
               </div>  
               )}
             </div>
