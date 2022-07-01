f48c70a351b7e91d63d6f232f20dc1b1b1351e56
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-07-01 09:57:58 +0530

    Single toggle template created

--- react-accordions/src/components/accordion2/SingleToggle.js
+++ react-accordions/src/components/accordion2/SingleToggle.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import listArray from "../../utils/data.json";
 
 export default class SingleToggle extends Component {
+    state = {
+        lists: []
+    }
+    componentDidMount() {
+        this.setState({
+            lists: listArray.map(list => ({ ...list, open: false }))
+        })
+    }
+    selectedRowHandler=(row)=> {
+      this.setState({
+          lists: this.state.lists.map(list => list.id === row.id ? ({...list, open: true }): list)
+      })
+    }
     render() {
         return (
             <div>
-                SingleToggle
+              {this.state.lists.map(list => 
+              <div key={list.id} onClick={() => this.selectedRowHandler(list)}>
+                 <div>{list.title}</div>
+                 {list.open && <div>Opened</div>} 
+              </div>  
+              )}
             </div>
         )
     }
