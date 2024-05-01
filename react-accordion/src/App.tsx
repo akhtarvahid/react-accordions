30bc31e2fa6b0a12b93d9c79d816f2dfe15baa2b
Vahid Akhtar <a845260@yara.com>
2024-05-01 17:22:11 +0530

    accordions grouped

--- react-accordion/src/App.tsx
+++ react-accordion/src/App.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import './App.css';
-import Accordion1 from './components/accordion1/SingleOpen';
-import SingleToggle from './components/accordion2/SingleToggle';
-import Accordion3 from './components/accordion3/Accordion3';
+import React, { useState } from "react";
+import "./App.css";
+import Accordion1 from "./components/accordion1/SingleOpen";
+import SingleToggle from "./components/accordion2/SingleToggle";
+import Accordion3 from "./components/accordion3/Accordion3";
 
 const App: React.FC = () => {
+  const [singleOpen, setSingleOpen] = useState(false);
+  const [singleToggle, setSingleToggle] = useState(false);
+  const [multipleInner, setMultipleInner] = useState(false);
   return (
     <div className="App">
-      <h3>Open one at once</h3>
-      <Accordion1 />
+      <h1>Accordions</h1>
+      <div className="row">
+        <h3>Open one at once</h3>
+        <button onClick={() => setSingleOpen((open) => !open)}>Sample</button>
+      </div>
+      {singleOpen && <Accordion1 />}
 
-      <h3>Open or close one at once</h3>
-      <SingleToggle />
+      <div className="row">
+        <h3>Open or close one at once</h3>
+        <button onClick={() => setSingleToggle((open) => !open)}>Sample</button>
+      </div>
+      {singleToggle && <SingleToggle />}
 
-      <h3>Select inner value from multiple accordions</h3>
-      <Accordion3 />
+      <div className="row">
+        <h3>Select inner value from multiple accordions</h3>
+        <button onClick={() => setMultipleInner((open) => !open)}>
+          Sample
+        </button>
+      </div>
+      {multipleInner && <Accordion3 />}
     </div>
   );
-}
+};
 
 export default App;
